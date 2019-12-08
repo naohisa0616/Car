aa05d756d45c334adcad4e25598205e3f98698f2
naohisa0616 <naohisa0616@icloud.com>
2019-12-08 15:53:11 +0900

    クラスプロパティを定義する ・クラスプロパティを宣言する

--- Car/ViewController.swift
+++ Car/ViewController.swift
@@ -8,6 +8,35 @@
 
 import UIKit
 
+//書式　クラスプロパティ（タイププロパティ）の宣言
+//static let 定数名:型
+//static var 変数名:型
+
+class Car {
+//  クラスプロパティ
+    static var count = 0
+   
+//  インスタンスメンバーの定義
+//  インスタンスプロパティ
+    var moving = false
+    
+//  インスタンスメソッド
+    func start() {
+//      スタートした車をカウントアップします
+        Car.count += 1
+        moving = true
+    }
+    
+    func stop() {
+        if Car.count > 0 {
+//        止まったらカウントダウンします
+            Car.count -= 1
+            moving = false
+        }
+    }
+    
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
